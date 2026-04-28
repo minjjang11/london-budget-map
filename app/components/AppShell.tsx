6780e3b487ccfd77b1ee6a22c7bcec89ffd0a506
minjjang11 <chae990616@gmail.com>
2026-04-28 23:30:45 +0100

    Add

--- app/components/AppShell.tsx
+++ app/components/AppShell.tsx
@@ -22,16 +22,16 @@ export function AppShell({ children }: { children: React.ReactNode }) {
         <div className="fixed inset-0 z-[9999] overflow-hidden bg-[#FCFFFF]">
           <div className="absolute inset-0 bg-[#FCFFFF]" />
           <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center">
-            <svg aria-hidden viewBox="0 0 92 128" className="h-[96px] w-auto select-none">
+            <svg aria-hidden viewBox="0 0 100 140" className="h-[104px] w-auto select-none">
               <defs>
-                <linearGradient id="splash-pin-gradient" x1="46" y1="0" x2="46" y2="128" gradientUnits="userSpaceOnUse">
+                <linearGradient id="splash-pin-gradient" x1="50" y1="0" x2="50" y2="140" gradientUnits="userSpaceOnUse">
                   <stop offset="0%" stopColor="#00EDC6" />
-                  <stop offset="66%" stopColor="#00C899" />
+                  <stop offset="68%" stopColor="#00C899" />
                   <stop offset="100%" stopColor="#6A71FF" />
                 </linearGradient>
               </defs>
               <path
-                d="M46 0C72 0 90 19 90 45C90 66 79 79 67 92L46 128L25 92C13 79 2 66 2 45C2 19 20 0 46 0Z"
+                d="M50 0C78 0 100 22 100 50C100 74 83 90 68 108L50 140L32 108C17 90 0 74 0 50C0 22 22 0 50 0Z"
                 fill="url(#splash-pin-gradient)"
               />
             </svg>
